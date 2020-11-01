b57863520dc792bba0a4d1591ff74fbdf01ab4f9
T. Sambit Suranjan <32041421+CriticalHunter@users.noreply.github.com>
2020-11-01 11:04:32 +0530

    Add files via upload

--- scrape_knappily.py
+++ scrape_knappily.py
@@ -12,8 +12,8 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 
-res_cate = ['Technology','Society','On%20this%20day','Environment','Politics','Law','Burning%20Issues','Business','World','Ethics','Sports','Literature','Lifestyle',]
-category = ['Personalities']
+res_cate = []
+category = ['Technology','Society','On%20this%20day','Environment','Politics','Law','Burning%20Issues','Business','World','Ethics','Sports','Literature','Lifestyle','Personalities']
 
 for item in category:
     url = "https://knappily.com/"+item
